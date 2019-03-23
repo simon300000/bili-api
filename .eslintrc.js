38d09c37294425d93937369cf0f2ffdb2eb451ae
simon3000 <simon3000@163.com>
2019-03-24 04:47:57 +0800

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parserOptions: {},
+  env: {
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  plugins: [],
+  rules: {
+    'space-before-function-paren': 'off',
+    'generator-star-spacing': 'off'
+  }
+}
